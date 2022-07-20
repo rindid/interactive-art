aac4890371535167cd91a88912b8e3f2105ef110
rindid <48424638+rindid@users.noreply.github.com>
2022-07-20 22:52:39 +0900

    demo line

--- demo-curtain/sketch/sketch.js
+++ demo-curtain/sketch/sketch.js
@@ -1,27 +1,28 @@
 let beadsCurtains = [];
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight*(0.996));
 }
 
 function draw() {
   background(220);
-  beadsCurtains.push(new beadsCurtain());
+  beadsCurtains.push(new beadsCurtain(200));
   for (let nowBeads of beadsCurtains){
     nowBeads.display();
   }
 }
 
-function beadsCurtain() {
-  this.posX = 200;
-  this.posY = 10;
+function beadsCurtain(posX) {
+  this.posX = posX;
+  this.posY = height/2;
   this.size = 3;
 
-  this.update = function() {
+  /*this.update = function() {
     ;
-  };
+  };*/
 
   this.display = function() {
-    ellipse(this.posX, this.posY, this.size);
+    for(var y= 0;  y < height; y+=40)
+      ellipse(this.posX, y, this.size);
   };
 }
