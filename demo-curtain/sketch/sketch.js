8bf8ff35446cb273dc3876e45d9cb4a3044bf291
rindid <48424638+rindid@users.noreply.github.com>
2022-07-20 20:16:36 +0900

    demmmo

--- demo-curtain/sketch/sketch.js
+++ demo-curtain/sketch/sketch.js
@@ -1,8 +1,27 @@
+let beadsCurtains = [];
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
-  ellipse(50,50,80,80);
+  beadsCurtains.push(new beadsCurtain());
+  for (let nowBeads of beadsCurtains){
+    nowBeads.display();
+  }
+}
+
+function beadsCurtain() {
+  this.posX = 200;
+  this.posY = 10;
+  this.size = 3;
+
+  this.update = function() {
+    ;
+  };
+
+  this.display = function() {
+    ellipse(this.posX, this.posY, this.size);
+  };
 }
