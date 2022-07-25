86d24c6aaeb293a1a2b252085ee031136a88227d
rindid <48424638+rindid@users.noreply.github.com>
2022-07-26 02:08:06 +0900

    커튼 부분 좀 더 수정

--- demo-curtain/sketch/sketch.js
+++ demo-curtain/sketch/sketch.js
@@ -1,12 +1,15 @@
 let beadsCurtains = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight*(0.996));
+  let cnv = createCanvas(displayWidth/2, displayHeight*(0.888));
+  cnv.center('horizontal');
 }
 
 function draw() {
   background(220);
-  beadsCurtains.push(new beadsCurtain(200));
+  for(var x=50; x<width; x+=50){
+    beadsCurtains.push(new beadsCurtain(x));
+  }
   for (let nowBeads of beadsCurtains){
     nowBeads.display();
   }
@@ -16,13 +19,35 @@ function beadsCurtain(posX) {
   this.posX = posX;
   this.posY = height/2;
   this.size = 3;
+  this.interval = 3;
 
   /*this.update = function() {
     ;
   };*/
 
   this.display = function() {
-    for(var y= 0;  y < height; y+=40)
-      ellipse(this.posX, y, this.size);
+    var posY=0;
+    var y=0;
+    for(y=0;  y < (height-200)/40; y+=1){
+      posY=y*40;
+      noStroke();
+      fill('rgba(7,137,220,1.0)');
+      if(y%3==0){
+        ellipse(this.posX, posY+this.size/2, this.size);
+        posY=posY+this.size;
+      }
+      else if(y%3==1){
+        ellipse(this.posX, posY+(this.size+1)/2, this.size+1);
+        posY=posY+this.size+1;
+      }
+      else if(y%3==2){
+        ellipse(this.posX, posY+(this.size+2)/2, this.size+2);
+        posY=posY+this.size+2;
+      }
+      stroke(0);
+      line(this.posX, posY+this.interval, this.posX, (y+1)*40-this.interval);
+    }
+    noStroke();
+    ellipse(this.posX, y*40+(this.size+5)/2, this.size+5);
   };
 }
